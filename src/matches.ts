da3fa8d0d5102bb1e15c4b1c86a21f0b1534bf2d
Benjamin Grosse <benja@ideaknow.com>
2014-04-24 14:07:37 +0200

    fix submitting wrong timestamp for new matches

--- src/matches.ts
+++ src/matches.ts
@@ -28,20 +28,25 @@ module matches {
 	export function MatchCreateCtrl($scope: IMatchCreateScope, $http: ng.IHttpService, $location: ng.ILocationService) {
 
 		$scope.dateDate = new Date();
+		$scope.dateDate.setHours(0);
+		$scope.dateDate.setMinutes(0);
+		$scope.dateDate.setSeconds(0);
+		$scope.dateDate.setMilliseconds(0);
 		$scope.dateTime = new Date();
 
 		$scope.submit = function() {
 			if (!$scope.winner || !$scope.loser || !$scope.result) {
 				return alert('Debes rellenar todo');
 			}
+
+			var timestamp = $scope.dateDate.getTime() + ($scope.dateTime.getHours() * 3600000 + $scope.dateTime.getMinutes() * 60000);
+
 			var data:matches.INewMatch = {
-				date: (new Date($scope.dateDate.getTime() + ($scope.dateTime.getHours() * 3600000 + $scope.dateTime.getMinutes() * 60000))).toISOString(),
+				date: (new Date(timestamp)).toISOString(),
 			 	winner: $scope.winner,
 			 	loser: $scope.loser,
 			 	result: $scope.result
 			};
-
-			console.log(JSON.stringify(data));
 
 			$http.post('api/matches', data).success(function() {
 				$location.path('matches');
